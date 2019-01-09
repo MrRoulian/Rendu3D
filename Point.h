3fd4ea1d8aa9aa6fe79dacd1770bfb3c58a9fbd3
Julien <julien.bailly4@etu.univ-lorraine.fr>
2019-01-09 12:50:57 +0100

    version pointillé

---
+++ Point.h
@@ -0,0 +1,9 @@
+struct Point {
+	int x;
+	int y;
+	int z;
+
+	Point(int x, int y, int z);
+
+	Point();
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "tgaimage.h"
+#include "Point.h"
 #include <iostream>
+#include <string>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -27,9 +35,70 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 }
 
+void triangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAImage &image, TGAColor color) {
+	line(x0, y0, x1, y1, image, color);
+	line(x1, y1, x2, y2, image, color);
+	line(x2, y2, x0, y0, image, color);
+}
+
+void drawPoint(int x0, int y0, TGAImage &image, TGAColor color) {
+	image.set(x0, y0, color);
+}
+
+void drawFile(string fileName, TGAImage &image, TGAColor colorcolor, int taille) {
+
+	ifstream fichier(fileName, ios::in);  // on ouvre le fichier en lecture
+	
+
+
+	if (fichier)  // si l'ouverture a réussi
+	{
+		string option;
+		float x, y, z;
+		int p1, p2, p3;
+		int indice = 0;
+		char trash;
+		int trashInt;
+		Point *tab = new Point[6000];
+
+		
+		fichier >> option >> x >> y >> z;
+
+		do {
+
+			x = x * (taille / 2) + taille / 2;
+			y = y * (taille / 2) + taille / 2;
+			Point p = Point(x, y, z);
+			tab[indice] = p;
+			indice++;
+			drawPoint(x, y, image, white);
+			fichier >> option >> x >> y >> z;
+
+
+		} while (option == "v");
+
+		/*while (option != "f") { 
+			getline(fichier, option);
+			cout << option << endl;
+		};*/
+
+		/*do {
+			fichier >> option >> p1 >> trash >> trashInt >> trash >> trashInt >> trash >> p2 >> trash >> trashInt >> trash >> trashInt >> trash >> p3 >> trash >> trashInt >> trash >> trashInt >> trash;
+			triangle(tab[p1].x, tab[p1].y, tab[p2].x, tab[p2].y, tab[p3].x, tab[p3].y, image, color);
+		} while (option == "f");*/
+
+
+
+		fichier.close();  // on ferme le fichier
+	}
+	else {
+		cout << "echec ouverture fichier" << endl;
+	}
+}
+
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white);
+    TGAImage image(1000, 1000, TGAImage::RGB);
+	drawFile("diablo3_pose.txt", image, white, 1000);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
